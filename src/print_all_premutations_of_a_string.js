96418883ee61ecfea68f3b354bb8f95d19ee897f
Boris Yen <yulinyen@gmail.com>
2017-03-06 15:57:05 +0800

    add print all premutations of a string

---
+++ src/print_all_premutations_of_a_string.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function preMutation(str, prefix=''){ 
+    if(str.length === 1){
+        console.log(prefix+str) ;
+    } else {
+        let charArr = str.split('') ;
+        for(let i=0; i<charArr.length; i++){
+            let clone = charArr.slice() ; 
+            let cur = clone.splice(i, 1) ;
+            preMutation(clone.join(''), prefix+cur[0]) ;
+        }
+    }
+}
+
+preMutation('abc') ;
